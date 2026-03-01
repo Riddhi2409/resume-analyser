efa64c1eb3b7904e16a47d1f38ca178a282e443c
Riddhi2409 <98161523+Riddhi2409@users.noreply.github.com>
2026-03-01 23:32:37 +0530

    Update failureHandler.java

--- src/main/java/com/ai/Resume/analyser/service/failureHandler.java
+++ src/main/java/com/ai/Resume/analyser/service/failureHandler.java
@@ -3,15 +3,12 @@ public void onAuthenticationFailure(HttpServletRequest request,
                                     HttpServletResponse response,
                                     AuthenticationException exception) throws IOException {
 
-    boolean isApi = request.getHeader("Accept") != null && request.getHeader("Accept").contains("application/json");
+    
 
-    if (isApi) {
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.getWriter().write("{\"error\":\"Invalid credentials\"}");
         response.getWriter().flush();
-    } else {
-        response.sendRedirect("/login"); // Thymeleaf
-    }
+    
 }
